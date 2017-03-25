69026e16ca36013d09244f5c2a78a9fbcb132fb2
tattagreis <simonthesourcerer@gmail.com>
2017-03-25 11:25:06 +0100

    Fixed techtree for 1.2.2

---
+++ BeyondMun/HibernateGUI.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System.Reflection;
+using System.Timers;
+using KSP.IO;
+using KSP.UI.Screens;
+using BeyondMun;
+
+[KSPAddon(KSPAddon.Startup.EveryScene, false)]
+public class HibernateGUI : MonoBehaviour
+{
+    internal static string assetFolder = "BeyondMun/Assets/";
+
+    public static ModuleHibernate callingModule;
+
+    private static String hibernationTime = "1";
+    private static bool breakWarp = false;
+
+    private static ApplicationLauncherButton CivPopButton = null;
+    static bool CivPopGUIOn = false;
+    internal bool CivPopTooltip = false;
+    private GUIStyle _windowstyle, _labelstyle;
+    private bool hasInitStyles = false;
+
+    private static ApplicationLauncherButton appButton = null;
+
+    /// <summary>
+    /// Awake this instance.  Pre-existing method in Unity that runs before KSP loads.
+    /// </summary>
+    public void Awake()
+    {
+        //Debug.Log (debuggingClass.modName + "Starting Awake()");//What I am using to debug
+        DontDestroyOnLoad(this);
+        GameEvents.onGUIApplicationLauncherReady.Add(OnAppLauncherReady);//when AppLauncher can take apps, give it OnAppLauncherReady (mine)
+        GameEvents.onGUIApplicationLauncherDestroyed.Add(OnAppLauncherDestroyed);//Not sure what this does
+    }
+
+    public void OnAppLauncherDestroyed()
+    {
+        if (appButton != null)
+        {
+            Cancel();
+            ApplicationLauncher.Instance.RemoveApplication(appButton);
+        }
+    }
+
+
+    /// <summary>
+    /// Raises the app launcher ready event.  Method to create an app button on the AppLauncher, as well as tell
+    /// what/how the GUI is loaded.
+    /// </summary>
+    public void OnAppLauncherReady()
+    {
+        InitStyle();
+        string iconFile = "HibernateIcon";//This is the name of the file that stores the mod's icon to be used in the appLauncher
+        if (HighLogic.LoadedScene == GameScenes.SPACECENTER && appButton == null)
+        {//i.e. if running for the first time
+            appButton = ApplicationLauncher.Instance.AddModApplication(
+                OnToggleTrue,                           //Run OnToggleTrue() when user clicks button
+                Cancel,                          //Run OnToggleFalse() when user clicks button again
+                null, null, null, null,                 //do nothing during hover, exiting, enable/disable
+                ApplicationLauncher.AppScenes.ALWAYS,   //When to show applauncher/GUI button
+                GameDatabase.Instance.GetTexture(assetFolder + iconFile, false));//where to look for mod applauncher icon
+                                                                                //Debug.Log (debuggingClass.modName + "Finishing Awake()");//What I am using to debug
+        }
+        CivPopGUIOn = false;
+    }
+
+
+    /// <summary>
+    /// Presumably what to do when the user opens/clicks the button.  Called from OnAppLauncherReady.
+    /// </summary>
+    public static void OnToggleTrue()
+    {
+        //Debug.Log (debuggingClass.modName + "Starting OnToggleTrue()");
+        CivPopGUIOn = true;//turns on flag for GUI
+                            //Debug.Log (debuggingClass.modName + "Turning on GUI");
+    }
+
+    public static void StartHibernate()
+    {
+        try
+        {
+            float time = (float)Int32.Parse(hibernationTime);
+            if (time > 0)
+            {
+                time = time * ModuleHibernate.TIME_ONE_HOUR * 6f; // days
+                callingModule.GoHibernate(time);
+            }
+        }
+        catch (FormatException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        CivPopGUIOn = false;
+    }
+
+    public static void Cancel()
+    {
+        CivPopGUIOn = false;
+    }
+
+    /// <summary>
+    /// I'm not sure what this is for...but it was already here and it seems to work.
+    /// </summary>
+    private void InitStyle()
+    {
+        _windowstyle = new GUIStyle(HighLogic.Skin.window);
+        _labelstyle = new GUIStyle(HighLogic.Skin.label);
+        hasInitStyles = true;
+    }
+
+    /// <summary>
+    /// OnGUI() is called by the game and every time it refreshes the GUI.  I just need it to check if the GUI is
+    /// enabled and if it is, show it.
+    /// </summary>
+    public void OnGUI()
+    {//Executes code whenever screen refreshes.  Extension to enable use of button along main bar on top-right of screen.
+        if (CivPopGUIOn)
+        {
+            PopulationManagementGUI();//If button is enabled, display rectangle.
+        }//end if
+    }//end OnGui extension
+
+    /// <summary>
+    /// This method controls how the window actually looks when the HUD window is displayed.
+    /// </summary>
+    void PopulationManagementGUI()
+    {
+        GUI.BeginGroup(new Rect(Screen.width / 2 - 250, Screen.height / 2 - 250, 500, 500));
+        GUILayout.BeginVertical("box");
+
+        GUIStyle style = new GUIStyle(GUI.skin.label);
+        style.alignment = TextAnchor.MiddleCenter;
+        GUILayout.Label("Probe Hibernation", style);
+
+        GUILayout.BeginHorizontal();           
+        GUILayout.Label("Sleep for days: ");
+        hibernationTime = GUILayout.TextField(hibernationTime, 12);
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(4);
+
+        GUILayout.BeginHorizontal();
+        breakWarp = GUILayout.Toggle(breakWarp, " Break warp on awake");
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(4);
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Cancel", GUILayout.Width(100f)))
+        {
+            Cancel();
+        }
+        if (GUILayout.Button("Hibernate", GUILayout.Width(100f)))
+        {
+            StartHibernate();
+        }
+
+        GUILayout.EndHorizontal();
+        GUILayout.EndVertical();
+        GUI.EndGroup();
+    }
+}
---
+++ BeyondMun/ModuleHibernate.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BeyondMun
+{
+    public class ModuleHibernate : PartModule
+    {
+
+        public const float TIME_ONE_MINUTE = 60;
+        public const float TIME_ONE_HOUR = TIME_ONE_MINUTE * 60;
+        public const float TIME_ONE_DAY = TIME_ONE_HOUR * 6;
+
+        public string[] STATE_TRANSLATION = new string[]
+        {
+            "BROKEN",
+            "SHUTTING DOWN",
+            "SLEEPING",
+            "BOOTING",
+            "ACTIVE"
+        };
+
+        public enum HybernationState
+        {
+            BROKEN,
+            SHUTTING_DOWN,
+            SLEEPING,
+            BOOTING,
+            ACTIVE
+        }
+        
+        HybernationState state = HybernationState.ACTIVE;
+
+        float progress = 0;
+        float sleepTime = 0;
+
+        float timeToBoot = 2 * TIME_ONE_HOUR;
+        float energyReductionRate = 0.1f;
+
+        [KSPField(guiActive = true, guiName = "Hib. status")]
+        private string statusLine = "ACTIVE";
+
+
+        private ModuleCommand commandModule;
+        private ModuleResource ressourceModule;
+
+        private void SetStatus()
+        {
+            if (state == HybernationState.SHUTTING_DOWN || state == HybernationState.BOOTING)
+            {
+                statusLine = state.ToString() + "(" + GetReadableTimeString(progress) + ")";
+            }
+            else if (state == HybernationState.SLEEPING)
+            {
+                statusLine = state.ToString() + "(" + GetReadableTimeString(sleepTime) + ")";
+            } 
+            else
+            {
+                statusLine = state.ToString();
+            }
+        }
+
+        private string GetReadableTimeString(float time)
+        {
+            string readableString = "";
+            float days = (time / TIME_ONE_DAY);
+            if (days >= 1)
+            {
+                readableString = days.ToString("0.0") + "d ";
+            }
+            else if (time > TIME_ONE_HOUR)
+            {
+                readableString = (time / TIME_ONE_HOUR).ToString("0.0") + "h";
+            }
+            else if (time < TIME_ONE_HOUR)
+            {
+                readableString = (time / 60).ToString("0") + "m";
+            }            
+            return readableString;
+        }
+
+        /// <summary>
+        /// Called when the part is started by Unity.
+        /// </summary>
+        public override void OnStart(StartState state)
+        {
+            commandModule = part.Modules.GetModule<ModuleCommand>();
+            
+            foreach (ModuleResource res in commandModule.inputResources)
+            {
+                if (res.name == "ElectricCharge")
+                {
+                    ressourceModule = res;
+                    break;
+                }
+            }
+        }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+
+            switch (state)
+            {
+                case HybernationState.SLEEPING:
+                    commandModule.part.RequestResource("ElectricCharge", ressourceModule.rate * energyReductionRate * TimeWarp.deltaTime);
+                    sleepTime -= TimeWarp.deltaTime;
+                    if (sleepTime <= 0)
+                    {
+                        state = HybernationState.BOOTING;
+                        progress = timeToBoot;
+                    }
+                    break;
+
+                case HybernationState.SHUTTING_DOWN:
+                    commandModule.part.RequestResource("ElectricCharge", ressourceModule.rate * TimeWarp.deltaTime);
+                    progress -= TimeWarp.deltaTime;
+                    if (progress <= 0)
+                    {
+                        state = HybernationState.SLEEPING;
+                    }
+                    break;
+
+                case HybernationState.BOOTING:
+                    commandModule.part.RequestResource("ElectricCharge", ressourceModule.rate * TimeWarp.deltaTime);
+                    progress -= TimeWarp.deltaTime;
+                    if (progress <= 0)
+                    {
+                        state = HybernationState.ACTIVE;
+                        commandModule.minimumCrew = 0;
+                    }
+                    break;
+            }
+
+            SetStatus();
+        }
+
+        public void GoHibernate(float time)
+        {
+            if (commandModule != null)
+            {
+                state = HybernationState.SHUTTING_DOWN;
+                statusLine = state.ToString();
+                progress = timeToBoot;
+                sleepTime = time;
+                commandModule.minimumCrew = 1;
+            }
+        }
+
+        [KSPEvent(active = true, guiActive = true, guiActiveEditor = false, guiActiveUnfocused = false, guiName = "Hibernate")]
+        private void Hibernate()
+        {
+            HibernateGUI.callingModule = this;
+            HibernateGUI.OnToggleTrue();
+        }
+
+        public void Load(ConfigNode config)
+        {
+            base.Load(config);
+
+            if (float.TryParse(config.GetValue("timeToBoot"), out timeToBoot))
+            {
+                timeToBoot *= TIME_ONE_HOUR;
+            }
+
+            float.TryParse(config.GetValue("energyRate"), out energyReductionRate);
+
+            float.TryParse(config.GetValue("progress"), out progress);
+            float.TryParse(config.GetValue("sleepTime"), out sleepTime);
+
+            string stateString = config.GetValue("state");
+            if (stateString != null) { 
+                state = (HybernationState)Enum.Parse(typeof(HybernationState), stateString);
+            }
+        }
+
+        public virtual void Save(ConfigNode config)
+        {
+            base.Save(config);
+            config.AddValue("timeToBoot", Mathf.Round(timeToBoot / TIME_ONE_HOUR));
+            config.AddValue("energyRate", energyReductionRate);
+
+            config.AddValue("progress", progress);
+            config.AddValue("sleepTime", sleepTime);
+
+            config.AddValue("state", state.ToString());
+        }
+    }
+}
